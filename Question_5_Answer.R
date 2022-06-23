de4879deef42f9efdcf880daf95e763a795d7b50
Shideler <35582596+Shideler@users.noreply.github.com>
2022-06-23 12:54:25 -0600

    Steve updated Question_5_Answers

--- Question_5_Answer.R
+++ Question_5_Answer.R
@@ -8,6 +8,8 @@
 # PCR results relational database tables. The connecting sql key would be the sample ID and allow for
 #further report generation. 
 
+#Version 0.2 update - included some QC checks on the data frames NGS_df and pcr_df
+
 library(tidyverse)
 library(data.table)
 library(odbc)
@@ -29,6 +31,32 @@ NGS_df <- rbindlist(df_list)
 
 pcr_df <- read.csv("June_PCR_results.csv", header = TRUE, stringsAsFactors = FALSE)
 
+
+# Qc check of the NGS_df and pcr_df data frames. 
+qc_report_df <- data.frame()
+
+NGS_qc <- c(is.na(NGS_df), 
+            ncol(NGS_df), 
+            nrow(NGS_df),
+            length(unlist(unique(NGS_df$Sample_ID))))
+
+pcr_qc <- c(is.na(pcr_df), 
+            ncol(pcr_df), 
+            nrow(pcr_df),
+            length(unlist(unique(pcr_df$Sample_ID))))
+
+qc_report_df <- rbind(NGS_qc, pcr_qc)
+
+#checks to see that sample ID in NGS_df match pcr_df and vice versa
+
+ngs_sample_ID <- ifelse(all(NGS_df$Sample_ID %in% pcr_df$Sample_ID) == TRUE, TRUE, FALSE)
+pcr_sample_ID <- ifelse(all(pcr_df$Sample_ID %in% NGS_df$Sample_ID) == TRUE, TRUE, FALSE)
+
+qc_report_df$Sample_ID_Match <- c(ngs_sample_ID, pcr_sample_ID)
+
+#outputs the qc_report_df
+
+write.csv(qc_report_df, "ngs_pcr_qc_report.csv")
 
 #sets up an SQL connections
 sql_con <- dbConnect(odbc(),
